95d1385b3873b7e00af5fce53f1bd2ada6e0e608
divya1viswanath <79564191+divya1viswanath@users.noreply.github.com>
2022-03-05 20:22:51 +0530

    Update Knn.py

--- Knn.py
+++ Knn.py
@@ -1,10 +1,12 @@
-from sklearn.neighbour import kNeighboursClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 irisData=load_iris()
+print(irisData.data)
 x=irisData.data
 y=irisData.target
-x_train,x_test,y_train,y_test(x,y,testsize=0.2,random_state=42)
-Knn=kNeighboursClassifier(n_neighbour=7)
-Knn.fit(x_train,y_train)
-print(Knn.predict(x_test))
+print(y)
+x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=42)
+knn=KNeighborsClassifier(n_neighbors=7)
+knn.fit(x_train,y_train)
+print(knn.predict(x_test))
